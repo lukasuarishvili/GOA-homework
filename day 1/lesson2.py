a5337e51d7ad6bcb7d16132148522e41ebd0833c
lukasuarishvili <lukasuarishvili13@gmail.com>
2024-09-04 13:28:01 -0700

    first commit

---
+++ day 1/lesson2.py
@@ -0,0 +1,5 @@
+nama="luka"
+age="15"
+surname="suarishvili" 
+
+print("hello" +" "+"my"+" " + "name"+" " +"is"+" "+nama+" "+ surname+" "+"and" +" " "am"+" "+str(age)+"years"+" "+"old,"+" "+"i"+" "+ "currentle"+" "+"study"+" "+"at"+" "+ "GOA-academy.")
