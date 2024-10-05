486b7e63b15b8ee859a001726bd71a2f1922dc2a
lukasuarishvili <lukasuarishvili13@gmail.com>
2024-10-05 17:58:23 +0400

    commit name

---
+++ level002/homework.py
@@ -0,0 +1,24 @@
+score=10 
+print(score)
+health=100
+print(health)
+attacking_points =20
+print(attacking_points)
+defence_points =45
+print(defence_points)
+math_score=35
+print(math_score)
+english_score=23
+print(english_score)
+science_score=12
+print(science_score)
+chemistry_score= 23
+print(chemistry_score)
+history_score=67
+print(history_score)
+
+name="luka"
+surname="suarishvili"
+age=15
+
+print("hello"+" "+"am"+" "+name+" "+surname+" and am "+str(age)+"years old.")
---
+++ level004/level4_homework.py
@@ -0,0 +1,45 @@
+#firs_task
+name=input("hello whats you're name: ")
+
+last_name=input("you're lastname: ")
+
+age=input("and you're age: ")
+
+print(
+
+    
+
+)
+
+#seconde_task
+
+first=int(input("enter a number: "  ))
+seconde=int(input("enter a number:  "))
+sum=(first+seconde)
+
+print("the sum is:"  + str(sum) )
+
+#thirde_task
+
+user_1 = input("enter your name: ")
+
+print(user_1)
+
+#forth_task
+
+math = 20 + 20
+math_2 = math - 10
+math_3 = math * 2
+print(math_3)
+
+#fifthe_taske
+academy=input("what academy do you study: ")
+name=input("whatse you're name: ")
+last_name=input("whats you're' last name? ")
+age=input("whats you're age:")
+school=input("where do you study:")
+print(academy)
+print(name)
+print(last_name)
+print(age)
+print(school)
