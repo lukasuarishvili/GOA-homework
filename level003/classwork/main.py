16b544b706f6cbf04181cd0c4a40b458d45fcb86
lukasuarishvili <lukasuarishvili13@gmail.com>
2024-10-06 11:45:21 +0400

    commit name

---
+++ level003/classwork/main.py
@@ -0,0 +1,5 @@
+name="luka"
+player_speed=15
+surname="suarishvili"
+print(name)
+print(surname)
---
+++ level006/classwork/main.py
@@ -0,0 +1,11 @@
+#task1
+
+Name=str(input("whats you're name? "))
+surname=str(input("whats you're surname? "))
+print(Name+surname)
+
+#task2
+num1=int(input("first number?"))
+num2=int(input("seconde number?"))
+
+print(num1+num2, num1-num2,num1*num2,num1/num2,num1//num2,num1**num2)
