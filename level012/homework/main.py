7e7a354cd4bc2346030f4c55abcd2d580f7bc7ff
lukasuarishvili <lukasuarishvili13@gmail.com>
2024-10-20 15:22:49 +0400

    commit name

---
+++ level012/homework/main.py
@@ -0,0 +1,36 @@
+#task1
+for i in range(1, 50 , 2):
+     print("goa")
+
+
+i=50
+while i >0 :
+    print("goa")
+    i=i-1
+
+#task2
+size = 3  
+for z in range(size):
+    print("*" * size )  
+
+size=3
+z=0
+while z < size:
+    print("*" * 3 )
+    z=z+1
+
+#task3
+size = 3  
+for z in range(size):
+      print("*" * 4 )  
+
+size=3
+z=0
+while z < size:
+    print("*" * 4 )
+    z=z+1
+
+#task4 ?
+#task5 ?
+
+    
