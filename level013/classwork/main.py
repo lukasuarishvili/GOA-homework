0175a07522ee29c24caa461aeb2359be1dbc2275
lukasuarishvili <lukasuarishvili13@gmail.com>
2024-10-25 11:48:24 +0400

    commit name

--- level013/classwork/main.py
+++ level013/classwork/main.py
@@ -6,3 +6,12 @@ for i in name:
      result += i
 
 print(result)
+
+#task2
+num=int(input("whats the number? "))
+
+if num%2==0:
+     print("good")
+
+else:
+     print("bad")
--- level013/homework/main.py
+++ level013/homework/main.py
@@ -1,4 +1,4 @@
-#task1
+#task1 ?
 #name=input("whats you're name? ")
 #result=" "
 #s=" "
@@ -6,9 +6,35 @@
    # result+=i
 
 #task2
-num=int(input("what will be the start of iteraicia? "))
-sub=int(input("whats the end? "))
+#num=int(input("what will be the start of iteraicia? "))
+#sub=int(input("whats the end? "))
 
-for i in range( num , sub  ):
-    if i%2==0 and i%3==0:  
-     print(str(i)+"სამის და ორის ჯერადია")
+#for i in range( num , sub  ):
+   # if i%2==0 and i%3==0:  
+   #  print(str(i)+"სამის და ორის ჯერადია")
+
+#task3 ?
+
+#for i in range(5):
+  #  num=input("whats the number? ")
+   # result=num+i
+   # print(result)
+
+#task4
+#for i in range(-100, 100):
+   # if i>0: print(i)
+
+#task5
+num=int(input("მომეცი დადებითი ნომერი "))
+if num>0:
+    print("good")
+
+else :
+    num=input("მომეცი დადებითი ნომერი")
+    
+    if num>0:
+       print("good")
+
+    else:
+         num=input("მომეცი დადებითი ნომერი")
+    
