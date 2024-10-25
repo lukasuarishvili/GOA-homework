ea4a20bf1b63c9a5f7fa79a9a3edb261621bfb7a
lukasuarishvili <lukasuarishvili13@gmail.com>
2024-10-25 12:18:08 +0400

    commit name

---
+++ level015/homework/Main.py
@@ -0,0 +1,47 @@
+#task1
+num=int(input("give me a number under 10  "))
+if num>10:
+    print("number must over 10 ")
+    int(input("what's the number? "))
+
+else:
+    print("good job ")
+
+#task2
+num=int(input("give me ann odd number? "))
+if num%2==0:
+    print("good job")
+
+else: print("number must be an odd ")
+
+#task3
+num=int(input("give me a number? "))
+if num>0:
+    print("good job")
+
+else: print("number must be over 0 ")
+
+#task4
+num1=int(input("give me  first number? "))
+num2=int(input("give me seconde number? "))
+
+if num1==num2 :
+    print("good")
+
+else: print("bad job")
+
+#task5
+num1=int(input("give me a number wich will be over 100 and odd "))
+
+if num1%2==0 and num1>100:
+    print("good job")
+
+else: print("bad job")
+
+#task6
+num1=int(input("give me a number "))
+if num1%5==0 or num1%10==0:
+    print("good job")
+
+else:  print("bad job")
+    
