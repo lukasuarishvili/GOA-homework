93b8522dca1f2d7679fc166b5359d36f520abedc
lukasuarishvili <lukasuarishvili13@gmail.com>
2024-11-06 19:47:16 +0400

    commit name

--- level018/homework/main.py
+++ level018/homework/main.py
@@ -1,1 +1,11 @@
-# comming soon 
+#task1
+
+arr=["godfather","americanpsico","ironeman","lotr","h.p"]
+print(arr[0:5:4])
+
+#task2
+arr=[1,2,3,4,5,6,7,8,9,10]
+print(arr[0:10:4])
+#task3-comming soon
+#task4-comming soon
+#task5-comming soon
