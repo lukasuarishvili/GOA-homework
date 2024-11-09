fb570392a0a5d7cf7297b9d27bcb35e4c67b5aab
lukasuarishvili <lukasuarishvili13@gmail.com>
2024-11-09 19:55:04 +0400

    commit name

---
+++ level018/homework/main.py
@@ -0,0 +1,24 @@
+#task1
+
+arr=["godfather","americanpsico","ironeman","lotr","h.p"]
+print(arr[0:5:4])
+
+#task2-?
+
+arr=[1,2,3,4,5,6,7,8,9,10]
+print(arr[0:10:4])
+
+#task3
+
+arr=["datvi","mgeli","spiloo" ,"yvavi"]
+arr[0]="lomi"
+print(arr)
+
+#task4
+arr=["godfather","americanpsico","ironeman","lotr","h.p"]
+print(arr[-1])
+
+#task5
+arr=[10, 20, 30, 40, 50, 60]
+
+print(arr[:-3:-1])
