3fd2496293d82fa50527ed601c5ac6b32bf2e4ca
lukasuarishvili <lukasuarishvili13@gmail.com>
2024-11-13 19:52:31 +0400

    commit name

---
+++ level020/homework/main.py
@@ -0,0 +1,25 @@
+#task1
+arr=["ა","ე","ი","ო","უ","ფ","დ","ჯ"]
+print(arr[0:5:])
+
+#task2
+arr=[15,34,90,135,35,120,145,150,185,35,65,]
+
+for i in range(len(arr)):
+   if  arr[i]%3==0 and arr[i]%5==0:
+      i="good"
+      print(i)
+   else:
+    print(arr[i])
+       
+#task3
+
+arr=[1 , 4 ,"h" , 7 , "j" , 6 ,"r" ]
+
+for i in range(len(arr)):
+    
+    string=arr[i]
+print(string)
+ 
+       
+
