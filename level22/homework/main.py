872d8093676c7bd185f3e54f7132e64118c40bff
lukasuarishvili <lukasuarishvili13@gmail.com>
2024-11-20 18:18:37 +0400

    commit name

---
+++ level22/homework/main.py
@@ -0,0 +1,21 @@
+#task1
+
+names=["luka","davita","gio","nika","olak","luka"]
+counter=0
+for i in names:
+   print(i)
+   if names[i]=="luka":
+      counter+=1
+print(counter)
+
+
+#task2
+arr=[3,2,5,7,8,5,4,3,2,1,5,6,7,3,4,45,65,3,23,]
+new_arr=arr[-1:1:-1]
+print( new_arr )
+
+#task3
+list_of_numbers=[1,2,3,4,]
+
+for i in range(len(list_of_numbers)):
+    print(list_of_numbers)
