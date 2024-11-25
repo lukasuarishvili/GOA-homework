6e3a1a21d0886dba560c3daab7e317fe51b2ea7f
lukasuarishvili <lukasuarishvili13@gmail.com>
2024-11-25 21:00:26 +0400

    commit name

---
+++ level24/classwork/Main.py
@@ -0,0 +1,11 @@
+#task1
+num=10
+message="ილი კარგია ბიჭია მას უყუარს {} ჯერ აწნვლა ბურთის {} ჯერ კი ბურთის გახეტქვა".format(num,num+10)
+print(message)
+
+#task2
+
+texst_lom="შაურმა არ არის  ხინკალზე კაი"
+words=texst_lom.split()
+new_texs="?".join(words)
+print(new_texs)
