098fded50a5ff4c55c9acff87ca8a8094eb09057
lukasuarishvili <lukasuarishvili13@gmail.com>
2024-11-27 22:14:30 +0400

    commit name

--- level24/homework/main.py
+++ level24/homework/main.py
@@ -3,4 +3,14 @@ text = "Hello world, how are you?"
 result = text.split(" ")
 print(result)
 
-#tas2
+#task2
+words = ['Hello', 'world', 'how', 'are', 'you?']
+result = ' '.join(words)
+print(result)
+
+#task3
+text = "Hello, world! Welcome to the world!"
+new_text = text.replace("world", "universe")
+print(new_text)
+
+#task4
