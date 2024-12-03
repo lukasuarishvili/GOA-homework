c17884cab59935e38d14d75f0e42dea05cdab9e6
lukasuarishvili <lukasuarishvili13@gmail.com>
2024-12-03 18:10:23 +0400

    second commit

---
+++ level26/homework/main.py
@@ -0,0 +1,22 @@
+#task4 
+def print_patterns():
+    for _ in range(120):
+      
+        print("******")
+        print("******")
+        print("******")
+
+        print("     *")
+        print("    ***")
+        print("  *******")
+        print("***********")
+        print("     *")
+        print("     *")
+
+      
+        print("*******")
+        print(" *******")
+        print("  ********")
+        print("    ********")
+
+print_patterns()
