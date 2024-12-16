a05293906c2790626b9f575013f73a4f0bf21ac6
lukasuarishvili <lukasuarishvili13@gmail.com>
2024-12-16 21:02:04 +0400

    second commit

---
+++ level30/classwork/main.py
@@ -0,0 +1,11 @@
+#Highest and Lowest
+
+#In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+#Examples
+#Input: "1 23 4 5" => Output: "5 1"
+#Input: "1 2 -3 ... 
+list=[1,2,3,4,5]
+minimal=min(list)
+maxima=max(list)
+
+print(minimal, maxima)
