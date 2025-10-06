254d8e241b33e7c998aeffe4ebdeff5148780405
lukasuarishvili <lukasuarishvili13@gmail.com>
2025-10-06 17:13:53 +0400

    finished level160 classwork

--- group50/level160/classwork/src/components/App.jsx
+++ group50/level160/classwork/src/components/App.jsx
@@ -7,21 +7,31 @@ function App() {
   let [watchColor, setWatchColor] = useState("black");
   return (
     <>
-      <div className='w-full h-full flex items-center justify-center gap-5'>
+      <div className='w-full h-full flex lg:flex-row flex-col items-center justify-center gap-5 px-2 pb-2 mt-3'>
         <Watch color={watchColor} />
         <div className='flex flex-col gap-3'>
 
 
           <Info />
-          <div className='flex flex-col gap-2.5'>
+          <div className='flex flex-col gap-2.5 text-center lg:text-start items-center lg:items-start '>
             <h3 className='text-xl font-bold '>Select Color</h3>
-            <div className='flex gap-1.5'>
-              <button onClick={() => { setWatchColor("red") }} className='w-14 h-10 rounded bg-red-700'>red</button>
-              <button onClick={() => { setWatchColor("blue") }} className='w-14 h-10 rounded bg-blue-700' >blue</button>
-              <button onClick={() => { setWatchColor("black") }} className='w-14 h-10 rounded bg-gray-700'>black</button>
-              <button onClick={() => { setWatchColor("pink") }} className='w-14 h-10 rounded bg-pink-700'>pink</button>
-              <button onClick={() => { setWatchColor("skin") }} className='w-14 h-10 rounded bg-amber-200 '>skin</button>
+            <div className='flex gap-1.5 flex-wrap items-center'>
+              <button onClick={() => { setWatchColor("red") }} className='w-14 h-10 rounded bg-red-700 hover:bg-red-800'></button>
+              <button onClick={() => { setWatchColor("blue") }} className='w-14 h-10 rounded bg-blue-700 hover:bg-blue-800' ></button>
+              <button onClick={() => { setWatchColor("black") }} className='w-14 h-10 rounded bg-gray-700 hover:bg-gray-800'></button>
+              <button onClick={() => { setWatchColor("pink") }} className='w-14 h-10 rounded bg-pink-700 hover:bg-pink-800'></button>
+              <button onClick={() => { setWatchColor("skin") }} className='w-14 h-10 rounded bg-amber-200 hover:bg-amber-300 '></button>
             </div>
+          </div>
+          <div className='flex flex-col gap-4  items-center lg:items-start '>
+            <h3 className='text-xl font-bold'>Fetures</h3>
+            <div className='flex gap-3'>
+              <button className='bg-gray-200 hover:bg-gray-300 px-3 py-1 rounded'>Time</button>
+              <button  className='bg-gray-200 hover:bg-gray-300 px-3 py-1 rounded' >Heart Rate</button>
+            </div>
+            <button className='mt-2 bg-amber-500 hover:bg-amber-600 px-3.5 py-1.5 font-semibold rounded'>
+              BUY NOW
+            </button>
           </div>
         </div>
       </div>
--- group50/level160/classwork/src/components/Info.jsx
+++ group50/level160/classwork/src/components/Info.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Info() {
     return (
-        <div className='w-fit flex flex-col gap-2 max-w-2xl'>
+        <div className='w-fit flex flex-col gap-2 max-w-2xl text-center lg:text-start'>
             <h1 className='text-3xl tracking-[2px] font-bold '>FItBIT 19 - The Smartest Watch</h1>
             <p className='text-lg font-semibold text-gray-500 max-w-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus illum quasi  ! Fugiat labore </p>
 
