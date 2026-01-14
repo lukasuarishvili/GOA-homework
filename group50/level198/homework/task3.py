bbe46bfd6245d8f5dc096bf5d5ce1f988d2e4690
lukasuarishvili <lukasuarishvili13@gmail.com>
2026-01-14 20:17:23 +0400

    level 198 finished

--- group50/level198/homework/task3.py
+++ group50/level198/homework/task3.py
@@ -1,1 +1,7 @@
 # 3) მოცემულია სია nums = [1, 2, 3, 4, 5, 6], filter -> map ის გამოყენებით დატოვე ლუწი რიცხვები და თითოეული გაამრავლე 2-ზე
+
+nums = [1, 2, 3, 4, 5, 6]
+
+
+print(list(  map( lambda num: num*2   ,filter( lambda num: num%2==0 , nums))))
+
--- group50/level198/homework/task5.py
+++ group50/level198/homework/task5.py
@@ -1,1 +1,9 @@
 # 5) nums = [9, 2, 7, 4, 5, 6, 1], დატოვე მხოლოდ კენტი რიცხვები დაა დაალაგე ზრდადობით
+
+nums = [9, 2, 7, 4, 5, 6, 1]
+
+
+cvladi= lambda numLust : filter( lambda num: num if num%2==1 else None, sorted(numLust))
+
+
+print(list(cvladi(nums)))
